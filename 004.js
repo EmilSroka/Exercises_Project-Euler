470b67627959bef70842c10ff32a90a99bf19d60
Emil Sroka <srokaemil@gmail.com>
2019-12-30 11:45:31 +0100

    Add solution to 4 problem

---
+++ 004.js
@@ -0,0 +1,20 @@
+function largestPalindromeProduct(n) {
+    let maxNumber = 10 ** n - 1;
+    let minNumber = 10 ** (n - 1);
+    let palindromes = [];
+    for(let i=maxNumber; i>= minNumber; i--){
+        for(let j=maxNumber; j>= minNumber; j--){
+            if(isPalindrome(i*j)){
+                palindromes.push(i*j);
+            }
+        }
+    }
+    return Math.max(...palindromes);
+}
+  
+
+function isPalindrome(number){
+    let asString = String(number);
+    let reversed = [...asString].reverse().join('');
+    return asString === reversed;
+}
